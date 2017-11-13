975311f1184a1ca196610420f2e86db091051e66
Daron Adkins <daron.adkins@gmail.com>
2017-11-13 16:47:40 -0600

    pagination

--- src/FacebookPhotoGallery.php
+++ src/FacebookPhotoGallery.php
@@ -12,31 +12,41 @@ class FacebookPhotoGallery
         $this->pageId      = FACEBOOK_PAGE_ID;
         $this->client      = new Client(['base_uri' => 'https://graph.facebook.com/v2.11/']);
     }
-    public static function albums($after = null)
+    public function albums($limit = null, $before = null, $after = null)
     {
         $fields = 'name,link,created_time,cover_photo{picture,images.limit(1)}';
 
         $response = $this->client->request(
             'GET',
-            $this->pageId.'/albums?fields='.$fields. '&after='. $after . '&access_token='.$this->accessToken
+            $this->pageId .
+            '/albums?fields=' . $fields .
+            '&access_token=' . $this->accessToken .
+            '&before=' . $before .
+            '&after=' . $after .
+            '&limit=' . $limit
         );
 
         $results = json_decode($response->getBody());
 
-        return $results->data;
+        return $results;
     }
 
-    public function albumPhotos($albumId, $after)
+    public function albumPhotos($albumId, $limit = null, $before = null, $after = null)
     {
         $fields = 'photo_count,photos{images,name}';
 
         $response = $this->client->request(
             'GET',
-            $albumId . '?fields=' . $fields . '&after=' . $after . '&accessToken=' . $this->accessToken
+            $albumId .
+            '/photos?fields=' . $fields .
+            '&accessToken=' . $this->accessToken .
+            '&before=' . $before .
+            '&after=' . $after .
+            '&limit=' . $limit
         );
 
         $results = json_decode($response->getBody());
 
-        return $results->data;
+        return $results;
     }
 }
