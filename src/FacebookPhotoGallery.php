dfed353cff578106eb673eeec9803add6db52c7b
Daron Adkins <daron.adkins@gmail.com>
2017-11-13 14:31:21 -0600

    album photo functionality

--- src/FacebookPhotoGallery.php
+++ src/FacebookPhotoGallery.php
@@ -5,17 +5,34 @@ use GuzzleHttp\Client;
 
 class FacebookPhotoGallery
 {
-    public static function albums()
+
+    public function __construct()
     {
-        $accessToken = FACEBOOK_ACCESS_TOKEN;
-        $pageId      = FACEBOOK_PAGE_ID;
-        $fields      = 'name,link,created_time,cover_photo{picture,images.limit(1)}';
+        $this->accessToken = FACEBOOK_ACCESS_TOKEN;
+        $this->pageId      = FACEBOOK_PAGE_ID;
+        $this->client      = new Client(['base_uri' => 'https://graph.facebook.com/v2.11/']);
+    }
+    public static function albums($after = null)
+    {
+        $fields = 'name,link,created_time,cover_photo{picture,images.limit(1)}';
 
-        $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.11/']);
-
-        $response = $client->request(
+        $response = $this->client->request(
             'GET',
-            $pageId.'/albums?fields='.$fields.'&access_token='.$accessToken
+            $this->pageId.'/albums?fields='.$fields. '&after='. $after . '&access_token='.$this->accessToken
+        );
+
+        $results = json_decode($response->getBody());
+
+        return $results->data;
+    }
+
+    public function albumPhotos($albumId, $after)
+    {
+        $fields = 'photo_count,photos{images,name}';
+
+        $response = $this->client->request(
+            'GET',
+            $albumId . '?fields=' . $fields . '&after=' . $after . '&accessToken=' . $this->accessToken
         );
 
         $results = json_decode($response->getBody());
